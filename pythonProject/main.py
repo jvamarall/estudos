2ee6b3a3f13e39e4588147b625aa0c24b275eb2e
João Amaral <161532781+jvamarall@users.noreply.github.com>
2024-03-22 18:25:06 -0300

    Add files via upload
    Eu aprendi variáveis e tipos de dado.

---
+++ pythonProject/main.py
@@ -0,0 +1,73 @@
+#INTEGER
+
+age = 21
+players = 2
+quantity = 5
+
+print(f"You are {age} years old")
+print(f"There are {players} players online")
+print(f"You would like to buy {quantity} items")
+
+#FLOAT
+
+gpa = 3.2
+distance = 2.5
+price = 10.99
+
+print(f"your gpa is {gpa}")
+print(f"you ran {distance}Km")
+print(f"The price is ${price}")
+
+#STRING
+
+name = "Bro"
+food = "Pizza"
+email = "jvmeloamaral@gmail.com"
+
+print(f"Hello, {name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+
+
+#BOOLEAN
+
+online = True
+for_sale = False
+running = False
+
+print(f"Are you online?: {online}")
+print(f"Is the item for sale?: {for_sale}")
+print(f"Game running: {running}")
+
+x, y, z = 1, 2, 3
+
+print(x)
+print(y)
+print(z)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
